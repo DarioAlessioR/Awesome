43c8b9d44950bb8bf10316d7a9f8a1e5852ffe2a
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-05-31 10:08:37 -0400

    add js file with add function for branch1 of  Awesome

--- index.js
+++ index.js
@@ -1,46 +1,96 @@
-/* FROM HERE ARRAY WITH BANDS FOR PROJECT */
+const books = [];
 
+const addBook = (ev) => {
+  ev.preventDefault();
+  const book = {
+    title: document.getElementById('title').value,
+    author: document.getElementById('author').value,
+  };
+  books.push(book);
+  document.forms[0].reset();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('btnAdd').addEventListener('click', addBook);
+});
+
+for (let i = 0; i < books.length; i += 1) {
+  document.getElementById('booksList').innerHTML = books[i].title;
+}
+
+
+localStorage.setItem('fullName', 'Paquita Salas');
+localStorage.setItem('age', 50);
+
+let name = localStorage.getItem('fullName');
+let age = localStorage.getItem('age');
+ 
+console.log(name, age); // Paquita Salas 50
+
+console.log('holaallllllllllll');
+console.log(books);
+
+console.log(books[0]);
+
+console.log('ddddddddddddll');
+
+
+
+/* FROM HERE ARRAY WITH BANDS FOR PROJECT */
+/*
 const concerts = [
   {
     band: 'Fugados Band',
     category: 'Pure rock and roll band',
-    description: 'Concert of best rock and roll band of Venezuela, to be held at Main Arena at prime time on July 16th. Concert will be curated for future video to be broadcasted via Youtube Live.',
+    description: 'Concert of best rock and roll band of Venezuela, to be held at
+    Main Arena at prime time on July 16th. Concert will be curated for future video
+     to be broadcasted via Youtube Live.',
     image: 'images/fugados10.jpeg',
   },
   {
     band: 'Fugados Indie Rock Band',
     category: 'Pure indie rock style band',
-    description: 'Concert of best indie rock band of Venezuela, to be held at Main Arena at prime time, on July 15th. Concert will be curated for future video to be broadcasted via Youtube Live.',
+    description: 'Concert of best indie rock band of Venezuela, to be held at
+    Main Arena at prime time, on July 15th. Concert will be curated for future
+    video to be broadcasted via Youtube Live.',
     image: 'images/fugados11.jpg',
   },
   {
     band: 'Fugados Soul Rock Band',
     category: 'Pure soul rock and roll band',
-    description: 'The concert of the award winning soul rock band of America, to be held at Main Arena at prime time on July 17th. Concert will be curated for future video to be broadcasted via Youtube Live.',
+    description: 'The concert of the award winning soul rock band of America, to be
+    held at Main Arena at prime time on July 17th. Concert will be curated for
+    future video to be broadcasted via Youtube Live.',
     image: 'images/fugados12.jpg',
   },
   {
     band: 'Fugados Electric Wave Band',
     category: 'Pure Electric Wave rock and roll band',
-    description: 'Concert of best Electric Wave rock band of Venezuela, to be held at the Gold Arena in prime time. Concert will be broadcasted alive via Youtube Live and funds will contribute to a humanitarian cause.',
+    description: 'Concert of best Electric Wave rock band of Venezuela, to be held
+    at the Gold Arena in prime time. Concert will be broadcasted alive via
+    Youtube Live and funds will contribute to a humanitarian cause.',
     image: 'images/fugados13.jpg',
   },
   {
     band: 'Arepa y Sushi Band',
     category: 'Venezuelan tipical music band with Japanese artist included',
-    description: 'The aclaimed and world famouse Venezuelan music band that has conquered Asia and set the standard for ecnit music in Japan. Concert to be held before Main Arena event on July 15th.',
+    description: 'The aclaimed and world famouse Venezuelan music band that has
+     conquered Asia and set the standard for ecnit music in Japan. Concert
+     to be held before Main Arena event on July 15th.',
     image: 'images/arepaysushi2.png',
   },
   {
     band: 'Universidad Gaitera',
-    category: 'Tipical gaita band from Venezuela, founded in 1983 in Puerto La Cruz, Anzoategui, Venezuela',
-    description: 'Concert of the famouse Universidad Gaitera Band, to be held before main event at Gold Arena on July 17th.',
+    category: 'Tipical gaita band from Venezuela, founded in 1983 in Puerto
+     La Cruz, Anzoategui, Venezuela',
+    description: 'Concert of the famouse Universidad Gaitera Band, to be held
+     before main event at Gold Arena on July 17th.',
     image: 'images/universidad.jpeg',
   },
 ];
 
 /* FROM HERE CODE TO INJECT FEATURED BANDS FOR CAPSTONE PROJECT */
-
+/*
 let listBands = '';
 
 for (let i = 0; i < concerts.length; i += 1) {
@@ -63,7 +113,7 @@ for (let i = 0; i < concerts.length; i += 1) {
 }
 
 /* FROM HERE CODE TO OPEN AND CLOSE MOBILE MENU FOR CAPSTONE PROJECT */
-
+/*
 const mobileMenuButtonOpen = document.querySelector('.mobile-menu-open');
 const mobileMenuButtonClose = document.querySelector('.mobile-menu-close');
 const mobileMenu = document.querySelector('nav');
@@ -79,3 +129,5 @@ mobileMenuButtonClose.addEventListener('click', () => {
   mobileMenu.classList.add('nav-initial');
   mobileMenuButtonOpen.style.opacity = 1;
 });
+
+*/
